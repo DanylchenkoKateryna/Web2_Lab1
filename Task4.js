f0e18f290a9fa8fc823aecfd543cd7c1a2287194
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2023-02-20 00:58:22 +0200

    Task4

---
+++ Task4.js
@@ -0,0 +1,17 @@
+function shuffle (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    // поміняти елементи місцями
+    // ми використовуємо для цього синтаксис "деструктивне присвоєння"
+    // докладніше про нього - в наступних розділах
+    // те ж саме можна записати як:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
+const arr = ['r', 44, 56, 78, 2, 3]
+for (let i = 0; i < 10; i++) {
+  console.log(shuffle(arr))
+}
